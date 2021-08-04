35054f69b8f8f941bbfb36431370b9f4f66c9f86
Omer Kalif <omer908@gmail.com>
2021-08-04 14:17:51 +0300

    add notes

--- Vertex.py
+++ Vertex.py
@@ -1,4 +1,3 @@
-
 class Vertex(object):
 
     def __init__(self, key):
--- main.py
+++ main.py
@@ -11,8 +11,11 @@ def run_minimum_blocks():
 
     s1 = "abaxxxababaxxxxab"
     s2 = "baxxxxababaxxxaba"
-    s1 = "abaxxxababaxxxxabtturffqsqq"
-    s2 = "baxxxxababaxxxabaurfqsqqftt"
+    # s1 = "abaxxxababaxxxxabtturffqsqq"
+    # s2 = "baxxxxababaxxxabaurfqsqqftt"
+    #
+    s1 = "2121"
+    s2 = "1212"
     mb = Minimun_Blocks.MinimumBlocks(s1, s2)
     print(mb.run())
 
